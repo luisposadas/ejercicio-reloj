ddcfc932e813277d310dc1660bdf254f73f9850a
Luis Posadas <luisposadasp96@outlook.es>
2017-08-23 11:00:33 -0500

    Intercambio inicial de proyecto

---
+++ Reloj.java
@@ -0,0 +1,23 @@
+
+public class Reloj
+{
+    private int horas;
+    private int minutos;
+    public Reloj(int horaInicial, int minInicial)
+    {
+        horas = horaInicial;
+        minutos = minInicial;
+        
+    }
+    public String dimeTuTiempo()
+    {
+        String cadResultado = "";
+        cadResultado = cadResultado + horas;
+        cadResultado += ":";
+        cadResultado += minutos;
+        return cadResultado;
+    }
+    
+    
+    
+}
